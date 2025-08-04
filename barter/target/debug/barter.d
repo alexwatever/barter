4588c81a983b62efe41901b4421decf9c3aeb530
Alex Watson <alex@coded.cc>
2025-08-04 22:51:56 +1000

    Remove target

--- barter/target/debug/barter.d
+++
@@ -1,1 +0,0 @@
-/Users/alex/dev/barter/barter/target/debug/barter: /Users/alex/dev/barter/barter/src/auth.rs /Users/alex/dev/barter/barter/src/main.rs
--- barter/target/debug/deps/barter-bd3435d05ba42abd.d
+++
@@ -1,6 +0,0 @@
-/Users/alex/dev/barter/barter/target/debug/deps/barter-bd3435d05ba42abd.d: src/main.rs src/auth.rs
-
-/Users/alex/dev/barter/barter/target/debug/deps/barter-bd3435d05ba42abd: src/main.rs src/auth.rs
-
-src/main.rs:
-src/auth.rs:
